c96cdb010564839d87e1fbd0feb612242c036af0
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2023-05-11 20:28:14 +0530

    Add files via upload

---
+++ stubhome.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>welcome</title>
+</head>
+<body>
+    <?php 
+    include('login1.php');?> 
+    <p>welcome-<?php echo $_SESSION['user'];?></p>   
+</body>
+</html>
